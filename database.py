dc9ab0024bb30fbb987e8fcda77afeccf8ccfe01
Rikuu <s2322065@stu.musashino-u.ac.jp>
2024-01-18 14:46:49 +0900

    ローカルデータをデータベースに保存

--- database.py
+++ database.py
@@ -1,52 +1,44 @@
-#ローカルデータ
 import sqlite3
 
 # DBファイル名
 db_name = 'test.sqlite'
 
-# DBに接続する（指定したDBファイル存在しない場合は，新規に作成される）
-con = sqlite3.connect
-
-# DBへの接続を閉じる
-con.close()
 # 1．DBに接続する
-con = sqlite3.connect
-# print(type(con))
+con = sqlite3.connect(db_name)  # You need to call the connect method with the database name
 
 # 2．SQLを実行するためのオブジェクトを取得
 cur = con.cursor()
 
-sql_create_table_gdp = 'CREATE TABLE cars(id int, name text, gdp real);'
+# 3．テーブルを作成するSQL
+sql_create_table_gdp = 'CREATE TABLE gdp(id int, name text, gdp real);'
 
 # 4．SQLを実行する
 cur.execute(sql_create_table_gdp)
 
-
-con.close()
-# 1．DBに接続する
-con = sqlite3.connect
-# print(type(con))
+# 1．DBに接続する（新しい接続を作成する）
+con = sqlite3.connect(db_name)
 
 # 2．SQLを実行するためのオブジェクトを取得
 cur = con.cursor()
 
-# 3．SQLを用意
-# データを挿入するSQL
-# INSERT INTO テーブル名 VALUES (列に対応したデータをカンマ区切りで);
+# 3．データを挿入するSQL
 sql_insert_many = "INSERT INTO gdp VALUES (?, ?, ?);"
 
-# データをリストで用意する
+# 4．データをリストで用意する
 cars_list = [
-    (1, 'japan', 1500),
-    (2, 'germany', 800),
-    (3, 'NSX-R', 1200),
+    (1, 'japan', 3931266),
+    (2, 'india', 225659),
+    (3, 'germany', 5120355),
+    (4, 'France', 4365898),
+    (5, 'Iran', 409121),
+    (6, 'Iceland', 6872764),
 ]
 
-# 4．SQLを実行
+# 5．SQLを実行
 cur.executemany(sql_insert_many, cars_list)
 
-# 5．コミット処理（データ操作を反映させる）
+# 6．コミット処理（データ操作を反映させる）
 con.commit()
 
-# 6．DBへの接続を閉じる
+# 7．DBへの接続を閉じる
 con.close()
